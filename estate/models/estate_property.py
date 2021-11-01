61417d100ba767f68d0876f2d07b8cae736fb3de
asb-odoo <asb@odoo.com>
2021-11-01 21:54:42 +0530

    [Add] Added Views

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,14 +5,16 @@ class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
     
-    
-    name = fields.Char(required=True)
+    # def test(self):
+    #     return fields.Datetime.now()
+
+    name = fields.Char(string="Property Name", default="Unknown", required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=lambda self: fields.Datetime.now(), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(copy=False, readonly=True)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -24,4 +26,4 @@ class EstateProperty(models.Model):
         ('east', 'East'),
         ('west', 'West')        
         ])
-        
+    active = fields.Boolean(default=True)
