6cd8db0f47252c424678ce719b3a574b28f27351
asb-odoo <asb@odoo.com>
2021-11-09 21:59:12 +0530

    Added the demo of one2many, many2one and many2many

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,47 @@
 
 from odoo import models, fields
 
+
+class Test(models.Model):
+    _name='test'
+    _description = 'Test'
+    # _rec_name = data
+
+    data = fields.Char()
+    pincode = fields.Integer()
+
+    # seld here will be recordset [ list of many records ]
+    def name_get(self):
+        res = []
+        for r in self:
+            res.append((r.id, r.data))
+        return res
+
+class EstatePropertOffer(models.Model):
+    _name = 'estate.property.offer'
+    _description = 'Estate Property Offer'
+
+    price = fields.Float()
+    status = fields.Selection([('accepted', 'Accepted'),('refuse', 'Refused')])
+    partner_id = fields.Many2one('res.partner')
+    property_id = fields.Many2one('estate.property')
+
+
+class EstatePropertyTag(models.Model):
+    _name = 'estate.property.tag'
+    _description = 'Estate Property Tag'
+
+    name = fields.Char()
+
+
+class EstatePropertyType(models.Model):
+    _name = 'estate.property.type'
+    _description = 'Estate Property Type'
+
+    name = fields.Char()
+
+
+
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
@@ -28,3 +69,9 @@ class EstateProperty(models.Model):
         ])
     active = fields.Boolean(default=True)
     image = fields.Image()
+    property_type_id = fields.Many2one('estate.property.type')
+    salesman_id = fields.Many2one('res.users')
+    buyer_id = fields.Many2one('res.partner')
+    test_id = fields.Many2one('test')
+    property_tag_ids = fields.Many2many('estate.property.tag')
+    property_offer_ids = fields.One2many('estate.property.offer', 'property_id')
