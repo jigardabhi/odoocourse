486f3b9d4ef54bfab34624cd9a2bd060522840de
asb-odoo <asb@odoo.com>
2021-11-11 21:51:53 +0530

    [ADD] Compute on relational field, inverse, onchange, hiding columns in tree

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -78,8 +78,37 @@ class EstateProperty(models.Model):
     test_id = fields.Many2one('test')
     property_tag_ids = fields.Many2many('estate.property.tag')
     property_offer_ids = fields.One2many('estate.property.offer', 'property_id')
-    total_area = fields.Integer(compute="_compute_area")
+    total_area = fields.Integer(compute="_compute_area", inverse="_inverse_area")
+    best_price = fields.Float(compute="_compute_best_price")
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline")
 
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        for record in self:
+            if record.garden:
+                record.garden_area = 10
+                record.garden_orientation = 'north'
+            else:
+                record.garden_area = 0
+                record.garden_orientation = None
+
+
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(record.date_availability, days=record.validity)
+            # date_availability
+
+
+    @api.depends('property_offer_ids.price')
+    def _compute_best_price(self): # Recordset [ Collection  of records]
+        for record in self:
+            max_price = 0
+            for offer in record.property_offer_ids:
+                if offer.price > max_price:
+                    max_price = offer.price
+            record.best_price = max_price
 
     @api.depends('living_area', 'garden_area')
     def _compute_area(self):
@@ -87,4 +116,6 @@ class EstateProperty(models.Model):
         for record in self:
             record.total_area = record.living_area + record.garden_area
 
-
+    def _inverse_area(self):
+        for record in self:
+            record.living_area = record.garden_area = record.total_area / 2
